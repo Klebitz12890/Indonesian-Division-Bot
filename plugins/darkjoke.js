0b12fa773b79f739bef7eb3cc22619a61a16ff43
Bayu Nofiza Alamsyah <94290017+ImByu@users.noreply.github.com>
2022-03-12 16:07:30 +0700

    Update darkjoke.js

--- plugins/darkjoke.js
+++ plugins/darkjoke.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 m.reply(wait)
-heum = await fetch(`https://api.lolhuman.xyz/api/meme/darkjoke?apikey=089510fd9369ede85213ea91`)
+heum = await fetch(`https://api.xteam.xyz/randomimage/dark?APIKEY=KurrXd`)
    if (!heum) throw json
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, 'Darkjoke By Official ID Bot', footer, 'NEXT', `${usedPrefix + command}`, m)
